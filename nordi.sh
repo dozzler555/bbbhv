a05a2f15f65c7ddc487aa05a8ca60faae87f607d
dozzler555 <80205889+dozzler555@users.noreply.github.com>
2021-07-16 21:29:43 +0530

    Create nordi.sh

---
+++ nordi.sh
@@ -0,0 +1,19 @@
+while true
+do
+	echo "Activating NordVPN....."
+	echo ""
+	country="$(shuf -n 1 -e France Belgium Spain Italy Germany Netherlands Switzerland)"
+	echo "Going with $country this time :D"
+	echo ""
+	nordvpn connect $country
+	megasync &
+	sleep 300
+	echo "5 mins..."
+	sleep 300
+	echo "10 mins..."
+	sleep 300
+	echo "15 mins..."
+	sleep 300
+	echo "20 mins..."
+	sleep 1
+done
